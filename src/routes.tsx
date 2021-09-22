8304e324275ee8051363e9c93099adba122eff4a
Thayerres Scarpini <thayerres@gmail.com>
2021-09-22 15:48:48 -0300

    feat: create routes and home page

---
+++ src/routes.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from './pages/Home';
+
+const Routes = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
+
+export default Routes 
